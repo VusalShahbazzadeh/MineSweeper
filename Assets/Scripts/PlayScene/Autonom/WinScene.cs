9b550ad98be064e02f73d063a0c87a5aa2d2dbfa
Vusal <vusal.shahbazzadeh@gmail.com>
2020-01-06 12:40:43 +0400

    Made game differentiate between complexity levels for best time

--- Assets/Scripts/PlayScene/Autonom/WinScene.cs
+++ Assets/Scripts/PlayScene/Autonom/WinScene.cs
@@ -5,6 +5,7 @@ public class WinScene : MonoBehaviour
 {
 	public StopWatch StopWatchReference;
 	public GameObject GameManagerReference;
+	public Text ComplexityLevelText;
 	public Text RecordedTimeText;
 	public Text BestTimeText;
 
@@ -16,10 +17,11 @@ public class WinScene : MonoBehaviour
 	public void WinCalculations()
 	{
 		int bestTime;
-		bestTime = PlayerPrefs.GetInt("BestTime");
+		bestTime = PlayerPrefs.GetInt("BestTime " + PlayerPrefs.GetInt("CellCount").ToString());
 		if ((bestTime > StopWatchReference.RecordedTime && StopWatchReference.RecordedTime !=0 )|| bestTime == 0)
-			 PlayerPrefs.SetInt("BestTime", (int)StopWatchReference.RecordedTime);
-		BestTimeText.text = GameManager.SecondsIntoTime(PlayerPrefs.GetInt("BestTime"));
+			 PlayerPrefs.SetInt("BestTime " + PlayerPrefs.GetInt("CellCount").ToString(), (int)StopWatchReference.RecordedTime);
+		BestTimeText.text = GameManager.SecondsIntoTime(PlayerPrefs.GetInt("BestTime " + PlayerPrefs.GetInt("CellCount").ToString()));
 		RecordedTimeText.text = GameManager.SecondsIntoTime((int)StopWatchReference.RecordedTime);
+		ComplexityLevelText.text = "Complexity Level is :  " + PlayerPrefs.GetInt("CellCount");
 	}
 }
