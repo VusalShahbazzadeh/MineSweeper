d4da4b553a381d6831fefea41e1f5b4404932d98
Vusal <vusal.shahbazzadeh@gmail.com>
2020-01-07 18:09:55 +0400

    Differentiate Best time between Mine-Cell ratios

--- Assets/Scripts/PlayScene/Autonom/WinScene.cs
+++ Assets/Scripts/PlayScene/Autonom/WinScene.cs
@@ -6,6 +6,7 @@ public class WinScene : MonoBehaviour
 	public StopWatch StopWatchReference;
 	public GameObject GameManagerReference;
 	public Text ComplexityLevelText;
+	public Text MineCellRatioText;
 	public Text RecordedTimeText;
 	public Text BestTimeText;
 
@@ -17,11 +18,12 @@ public class WinScene : MonoBehaviour
 	public void WinCalculations()
 	{
 		int bestTime;
-		bestTime = PlayerPrefs.GetInt("BestTime " + PlayerPrefs.GetInt("CellCount").ToString());
+		bestTime = PlayerPrefs.GetInt("BestTime " + PlayerPrefs.GetInt("CellCount").ToString()+":" + PlayerPrefs.GetInt("PercentageOfMines"));
 		if ((bestTime > StopWatchReference.RecordedTime && StopWatchReference.RecordedTime !=0 )|| bestTime == 0)
-			 PlayerPrefs.SetInt("BestTime " + PlayerPrefs.GetInt("CellCount").ToString(), (int)StopWatchReference.RecordedTime);
-		BestTimeText.text = GameManager.SecondsIntoTime(PlayerPrefs.GetInt("BestTime " + PlayerPrefs.GetInt("CellCount").ToString()));
+			 PlayerPrefs.SetInt("BestTime " + PlayerPrefs.GetInt("CellCount").ToString() + ":" + PlayerPrefs.GetInt("PercentageOfMines"), (int)StopWatchReference.RecordedTime);
+		BestTimeText.text = GameManager.SecondsIntoTime(PlayerPrefs.GetInt("BestTime " + PlayerPrefs.GetInt("CellCount").ToString() + ":" + PlayerPrefs.GetInt("PercentageOfMines")));
 		RecordedTimeText.text = GameManager.SecondsIntoTime((int)StopWatchReference.RecordedTime);
 		ComplexityLevelText.text = "Complexity Level is :  " + PlayerPrefs.GetInt("CellCount");
+		MineCellRatioText.text = "Mine-Cell ratio is :  " + PlayerPrefs.GetInt("PercentageOfMines") + "%";
 	}
 }
